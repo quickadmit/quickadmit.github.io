4fb3a8231628d7035bf80280a46d825dc7f5d247
Adam Patterson <adam@adamrt.com>
2023-04-29 13:11:22 -0700

    Show login button on mobile

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -25,6 +25,14 @@
               Get Access
             </a>
           </div>
+          <div class="md:hidden flex">
+            <a
+              href="https://app.quickadmit.com/login"
+              class="ml-8 whitespace-nowrap inline-flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white hover:from-purple-700 hover:to-indigo-700"
+            >
+              Login
+            </a>
+          </div>
         </div>
       </Popover>
     </header>
